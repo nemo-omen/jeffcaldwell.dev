628c895ab8c3aa749039db4a72d77fb73741c07c
nemo-omen <nemo.omen@gmail.com>
2021-07-01 20:35:31 -0500

    Replace logo & favicon

--- src/components/Intro.svelte
+++ src/components/Intro.svelte
@@ -4,20 +4,15 @@
 
 <article id="intro">
     <section class="intro_greeting">
-        <h2 class="greeting">I'm <a href="#">Jeff</a> and I make fast, accessible, good-looking websites.</h2>
-        <!-- <div class="avatar" aria-hidden="true">
-            <Avatar />
-        </div> -->
+        <h2 class="greeting">I'm <a href="https://twitter.com/trainingm0ntage">Jeff</a>.<br/> I make fast, accessible, good-looking websites.</h2>
     </section>
     <section class="intro_message">
-        <section class="message">
-            <p>I'm a web developer based in Texas who specializes in the design and development of sites and apps for small businesses, nonprofits, and people like you.</p>
-        </section>
+        <p class="message">I'm a web developer in Texas who specializes in the design and development of sites and apps for small businesses, nonprofits, and people like you.</p>
+    </section>
+    <section id="cta">
+        <a href="/hire" class="cta-button">Check Out My Services</a>
     </section>
 </article>
-<section id="cta">
-    <a href="/hire" class="cta-button">Check Out My Services</a>
-</section>
 
 
 
@@ -26,63 +21,47 @@
         display: flex;
         flex-direction: column;
         align-items: center;
-        background: var(--red);
+        // background: var(--red);
+        background: var(--darkblue);
         gap: var(--space);
-        padding-block: var(--space);
+        padding-bottom: var(--space);
         margin-bottom: var(--space);
     }
 	.intro_greeting {
-        width: 100%;
         display: flex;
         flex-direction: column;
         justify-content: center;
         align-items: center;
-        gap: var(--space-small);
-        margin-inline: auto;
-		background-color: var(--red);
 	}
 	.greeting {
-        width: var(--width-body);
         font-family: var(--sans);
         font-weight: 900;
-        max-width: 40ch;
-		font-size: clamp(var(--size-600), 5vw, var(--size-900));
+		font-size: clamp(var(--size-600), 5vw, var(--size-800));
 		letter-spacing: 0.5rem;
-        grid-area: Greeting;
         text-align: center;
-        order: 2;
         color: var(--whitish);
 	}
-	.avatar {
-        width: clamp(20vw, 80px, 30vw);
-        order: 1;
-	}
-
     .intro_message {
         width: 100%;
     }
     .message {
-        color: var(--blackish);
+        color: var(--whitish);
         width: var(--width-body);
         margin-inline: auto;
         text-align: center;
-    }
-
-    .message p {
-        max-width: 75ch;
+        font-size: clamp(var(--size-500), 3vw, var(--size-600));
+        max-width: 50ch;
     }
 
     a {
-        color: var(--darkblue);
-        text-decoration-thickness: 3px;
-        text-underline-offset: 1uto;
+        color: var(--red);
+        text-decoration-thickness: 1px;
+        text-underline-offset: auto;
         transition: all 300ms ease-out;
     }
 
     a:hover {
-        color: var(--darkblue);
         text-decoration-thickness: 6px;
-        // text-underline-offset: 10px;
     }
 
     #cta {
@@ -90,7 +69,7 @@
         justify-content: center;
     }
     .cta-button {
-        background-color: var(--green);
+        background-color: var(--red);
         color: var(--white);
         text-decoration: none;
         padding: 1rem;
@@ -111,19 +90,5 @@
 
     .cta-button:active {
         transform: scale(1.0);
-    }
-
-    @media screen and (min-width: 768px) {
-        .intro_greeting {
-            flex-direction: row;
-        }
-
-        .greeting {
-            order: 1;
-            flex-basis: 60vw;
-        }
-        .avatar {
-            order: 2;
-        }
     }
 </style>
--- src/components/Logo.svelte
+++ src/components/Logo.svelte
@@ -17,14 +17,24 @@
 
 </script>
 <div class="logo" on:mouseenter={setHoverStyles} on:mouseleave={setHoverStyles}>
-  <svg viewBox="0 0 121 102" xmlns="http://www.w3.org/2000/svg" class="logo-svg" aria-hidden="true" focusable="false">
+  <!-- <svg viewBox="0 0 121 102" xmlns="http://www.w3.org/2000/svg" class="logo-svg" aria-hidden="true" focusable="false">
     <g fill={tColor} class="logo-t">
       <path d="M100 1V26.9091H68.4303V101H32.7229V26.9091H1V1H100Z" />
     </g>
     <g fill={dotColor} class="logo-dot">
       <circle cx="100" cy="84" r="17"/>
     </g>
-  </svg>
+  </svg> -->
+  <svg viewBox="0 0 88 65" fill="none" xmlns="http://www.w3.org/2000/svg" class="logo-svg">
+    <g fill={tColor} class="logo-t">
+      <path d="M57.328 0.639999V37.888C57.328 46.464 54.768 53.12 49.648 57.856C44.528 62.592 37.136 64.96 27.472 64.96C20.368 64.96 14.672 63.712 10.384 61.216C6.15999 58.72 3.24799 55.296 1.64799 50.944C0.11199 46.592 -0.24001 41.664 0.59199 36.16L22.768 32.416C21.936 38.432 22.032 42.56 23.056 44.8C24.144 47.04 26 48.16 28.624 48.16C30.8 48.16 32.4 47.232 33.424 45.376C34.448 43.52 34.96 40.48 34.96 36.256V0.639999H57.328Z"/>
+    </g>
+    <g fill={dotColor} class="logo-dot">
+      <circle cx="75.5" cy="51.5" r="12.5"/>
+    </g>
+    </svg>
+    
+      
 </div>
 
 <style lang="scss">
@@ -32,7 +42,7 @@
     transition: all 300ms ease-out;
   }
   .logo-svg {
-    height: clamp(2rem, 4vw, 5rem);
+    height: clamp(2rem, 4vw, 4rem);
   }
 
   .logo-t, .logo-dot {
