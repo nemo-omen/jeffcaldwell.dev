f8504f4eaff3be32738d918f4e4e59591be5a7dc
nemo-omen <nemo.omen@gmail.com>
2021-07-04 18:15:39 -0500

    Stat loading posts

---
+++ src/routes/writing/index.json.js
@@ -0,0 +1,27 @@
+import { parse } from 'path';
+
+export const get = async() => {
+  const modules = import.meta.glob('./**/index.svx');
+
+  const posts = [];
+
+  await Promise.all(Object.entries(modules).map(async([file, module]) => {
+    const { metadata } = await module();
+
+    posts.push({
+      author: metadata.author,
+      created: metadata.created,
+      slug: parse(file).dir,
+      subtitle: metadata.subtitle,
+      title: metadata.title,
+    });
+  }));
+
+  posts.sort((a,b) => (a.created > b.created) ? -1 : 1);
+
+  return {
+    body: {
+      posts: posts,
+    },
+  }
+};
---
+++ src/routes/writing/index.svelte
@@ -0,0 +1,21 @@
+<script context="module">
+  export const load = async({ fetch }) => {
+    const response = await fetch('/writing.json');
+
+    const { posts } = await response.json();
+
+    return {
+      props: {
+        posts
+      }
+    }
+  }
+</script>
+
+<script>
+  export let posts;
+</script>
+
+{#each posts as post}
+  <h3>{post.title}</h3>
+{/each}
