89a610a576fb4e8f5393ad3359ba4641d4e3de81
nemo-omen <nemo.omen@gmail.com>
2021-07-08 11:34:47 -0500

    Play with image overlap & -webkit text-stroke

---
+++ src/components/IntroImage.svelte
@@ -0,0 +1,72 @@
+<script>
+
+</script>
+
+<section class="intro-image">
+  <div class="intro-image-border-top"></div>
+  <img class="image" src="/MeIllustration.svg" alt="Illustration of Jeff Caldwell.">
+  <div class="intro-image-border-bottom"></div>
+</section>
+
+<style>
+  .intro-image {
+    margin: 0;
+    z-index: 50;
+    position: absolute;
+    bottom: 0;
+    z-index: 10;
+    right: 0;
+    max-width: 29vw;
+    }
+
+    .intro-image-border-top {
+      z-index: -1;
+      border-top:8px solid var(--red);
+      position: absolute;
+      top: var(--space);
+      right: 0;
+      left: 0;
+      bottom: 0;
+    }
+    .intro-image-border-bottom {
+      position: absolute;
+      z-index: 10;
+      border: 8px solid var(--red);
+      border-top: none;
+      top: var(--space);
+      right: 0;
+      left: 0;
+      bottom: 0;
+    }
+
+    img { 
+      z-index: 5;
+    }
+
+    @media screen and (max-width: 463px) {
+      .intro-image {
+        position: absolute;
+        top: -1rem;
+        right: 10vw;
+        max-width: 35vw;
+        }
+    }
+
+    @media screen and (max-width: 603px) {
+        .intro-image {
+            max-width: 40vw;
+        }
+    }
+
+    @media screen and (max-width: 769px) {
+        .intro-image {
+            max-width: 37vw;
+        }
+    }
+
+    @media screen and (max-width: 1700px) {
+        .intro-image {
+            max-width: 32vw;
+        }
+    }
+</style>
--- src/components/partials/home/IntroSection.svelte
+++ src/components/partials/home/IntroSection.svelte
@@ -1,15 +1,16 @@
 <script>
-	
+    import IntroImage from '../../IntroImage.svelte'
 </script>
 
-<section id="intro" class="flow">
+<section id="intro" class="flow space__outer_block">
     <section class="intro_greeting">
         <h2 class="greeting">Jeff<br>Caldwell</h2>
         <h3 class="sub-greeting">Makes Websites.</h3>
     </section>
-    <section class="flex-absolute-graphic">
+    <!-- <section class="flex-absolute-graphic">
         <img src="/MeIllustration.svg" alt="Illustration of Jeff Caldwell.">
-    </section>
+    </section> -->
+    <IntroImage />
 </section>
 
 
@@ -19,20 +20,18 @@
         position: relative;
         display: flex;
         z-index: 20;
-        // background-image: url('/MeIllustration.svg');
-        // background-position: right bottom;
-        // background-size: contain;
-        // background-repeat: no-repeat;
         padding-block: var(--space);
     }
     .intro_greeting {
         z-index: 100;
     }
 	.greeting {
+        -webkit-text-stroke: 2px var(--red);
+        color: var(--whitish);
         font-size: clamp(var(--size-900), 8vw, 9.969rem);
         font-weight: 900;
         letter-spacing: .05em;
-        color: var(--red);
+        // color: var(--red);
         line-height: 1;
 	}
     .sub-greeting {
@@ -40,18 +39,7 @@
         font-weight: 900;
         letter-spacing: 0.25rem;
     }
-    .flex-absolute-graphic {
-        margin: 0;
-        z-index: 50;
-        position: absolute;
-        // top: calc(var(--space) * -1);
-        bottom: 0;
-        z-index: -1;
-        // bottom: calc((var(--space) * -1) + 1rem);
-        right: 0;
-        // max-width: clamp(155px, 25vw, 595px);
-        max-width: 29vw;
-    }
+    
     .message {
         font-size: clamp(var(--size-500), 3vw, var(--size-600));
         max-width: 50ch;
@@ -103,26 +91,8 @@
             background-size: 35vw;
             background-position: top right var(--space);
         }
-        .flex-absolute-graphic {
-            position: absolute;
-            top: -1rem;
-            right: 10vw;
-            max-width: 35vw;
-        }
         .sub-greeting {
             text-align: center;
-        }
-    }
-
-    @media screen and (max-width: 603px) {
-        .flex-absolute-graphic {
-            max-width: 40vw;
-        }
-    }
-
-    @media screen and (max-width: 769px) {
-        .flex-absolute-graphic {
-            max-width: 37vw;
         }
     }
 </style>
