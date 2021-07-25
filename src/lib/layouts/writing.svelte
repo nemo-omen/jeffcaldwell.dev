8ba7f6048c9bcdbc26092a4c46ab8ea774553525
nemo-omen <nemo.omen@gmail.com>
2021-07-25 12:05:02 -0500

    Adjust post-header styles

--- src/lib/layouts/writing.svelte
+++ src/lib/layouts/writing.svelte
@@ -8,23 +8,26 @@
 </script>
 
 
+<section class="post-header">
+  <h2 class="title">{title}</h2>
+  
+  <div class="sub-header">
+    <p class="post-meta">by {author} on {created}</p>
+    <nav class="nav-list">
+      {#each tags as tag}
+        <a href="/tag/{tag}" class="tag">{tag}</a>
+      {/each}
+    </nav>
+  </div>
+</section>
 <section class="flow post">
-  <section class="post-header">
-    <h2 class="title">{title}</h2>
-    
-    <div class="sub-header">
-      <h3 class="post-meta">by {author} on {created}</h3>
-      <nav class="nav-list">
-        {#each tags as tag}
-          <a href="/tag/{tag}" class="tag">{tag}</a>
-        {/each}
-      </nav>
-    </div>
-  </section>
   <slot></slot>
 </section>
 
 <style>
+  .post-header {
+    margin-block: var(--space);
+  }
   .post-header > * + *{
     margin-top: 0.5rem;
   }
@@ -37,16 +40,12 @@
     flex-wrap: wrap;
     justify-content: space-between;
     gap: var(--space-small);
-    margin-inline: 0.5rem;
   }
   .post-meta {
     font-weight: 500;
     font-size: var(--font-size-small);
-    color: var(--darkblue);
-    text-decoration: underline;
     color: var(--red);
-    text-underline-offset: 0.75rem;
-    text-decoration-thickness: 2px;
+    max-width: 50ch;
   }
   .nav-list {
     display: flex;
