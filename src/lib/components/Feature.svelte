b6a624ce65e6513c7e3fc2ab11fdd1bed25e904e
nemo-omen <nemo.omen@gmail.com>
2021-07-25 13:58:20 -0500

    Update work section and Feature commponent

---
+++ src/lib/components/Feature.svelte
@@ -0,0 +1,93 @@
+<script>
+  export let title;
+  export let subtitle;
+  export let image;
+  export let status;
+  export let link;
+</script>
+
+<article class="feature">
+  <div class="feature-text">
+    <header class="feature-header">
+      <h3 class="feature-title">{title}</h3>
+      <p class="feature-subtitle">{subtitle}</p>
+      <div class="feature-status">{status}</div>
+    </header>
+    <section class="feature-content">
+      <slot></slot>
+    </section>
+    <footer class="feature-footer">
+      <a href="{link}" class="feature-link">Check it Out</a>
+    </footer>
+  </div>
+  <div class="feature-image">
+    {#if image}
+    <img src="{image.url}" alt="{image.description}" width="1440" height="800">
+    {/if}
+  </div>
+</article>
+
+<style>
+  .feature {
+    display: flex;
+    flex-direction: column;
+    max-width: 100%;
+    gap: var(--space);
+  }
+  .feature-text {
+    flex: 1;
+    order: 2;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    padding-inline: var(--space);
+  }
+  .feature-image {
+    flex: 2;
+    order: 1;
+  }
+  .feature-image img {
+    max-width: 100%;
+    height: auto;
+  }
+  .feature-header {
+    display: flex;
+    flex-direction: column;
+    gap: var(--space-xsmall);
+  }
+  .feature-title {
+    color: var(--red);
+    font-size: var(--font-size-xl);
+    line-height: 1;
+  }
+  .feature-subtitle {
+    font-size: var(--font-size-normal);
+    font-family: var(--display);
+    letter-spacing: 0.125em;
+  }
+  .feature-status {
+    font-size: var(--font-size-small);
+    line-height: 1;
+    color: var(--secondary);
+  }
+  .feature-link {
+    color: var(--secondary);
+    transition: color 300ms ease-out;
+  }
+  .feature-link:hover {
+    color: var(--primary);
+  }
+
+  @media screen and (min-width: 768px) {
+    .feature-image {
+      max-width: 66%;
+    }
+    .feature {
+      flex-direction: row;
+    }
+    .feature-text {
+      padding-block: var(--space);
+      padding-inline: unset;
+    }
+  }
+</style>
--- src/lib/components/partials/home/ProjectsSection.svelte
+++ src/lib/components/partials/home/ProjectsSection.svelte
@@ -1,6 +1,6 @@
 <script>
   import FlexSwitcher from '$lib/components/FlexSwitcher.svelte';
-  import Card from '$lib/components/Card.svelte';
+  import Feature from '$lib/components/Feature.svelte';
 </script>
 
 <section id="work" class="width__body">
@@ -12,12 +12,21 @@
     </p>
   </header>
   <FlexSwitcher>
-    <Card link="/work/feedme" status="Ongoing" title="FeedMe" subtitle="The simple RSS reader">
+    <Feature
+      image={{url: '/images/FeedMe_Feature.png', description: 'FeedMe front page interface.'}}
+      status="July 2021 - Ongoing"
+      title="FeedMe"
+      subtitle="The easy RSS reader"
+      link="/work/feedme"
+    >
+    <p>FeedMe is being developed as an accessible and easy-to-use RSS reader.</p>
+    </Feature>
+    <!-- <Card link="/work/feedme" status="Ongoing" title="FeedMe" subtitle="The simple RSS reader">
       <p slot="content">
         This is a moderate amount of content that I'll use to take up some space temporarily.
       </p>
       <img src="/FeedMe-Screenshot.png" alt="FeedMe front page interface." slot="image">
-    </Card>
+    </Card> -->
     <!-- <Card link="/projects/feedme" status="Ongoing" title="FeedMe" subtitle="The simple RSS reader">
       <p slot="content">
         This is a moderate amount of content that I'll use to take up some space temporarily.
