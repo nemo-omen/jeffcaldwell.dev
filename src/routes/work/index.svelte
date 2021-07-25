2e2a779f7febbfceef96f7d75472b9b79017a227
nemo-omen <nemo.omen@gmail.com>
2021-07-25 17:06:12 -0500

    Remove work heading

--- src/routes/work/index.svelte
+++ src/routes/work/index.svelte
@@ -3,8 +3,8 @@
 </script>
 
 <section class="space__outer_top flow">
-  <header class="section-header">
+  <!-- <header class="section-header">
     <h2 class="section-title">Work</h2>
-  </header>
+  </header> -->
   <ProjectsSection />
 </section>
