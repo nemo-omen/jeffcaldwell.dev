4b9df6d0fa7baaf6d3a717568f7315c9264fe0d6
nemo-omen <nemo.omen@gmail.com>
2021-07-25 09:33:25 -0500

    Build & add gh-pages deploy script

--- svelte.config.js
+++ svelte.config.js
@@ -1,13 +1,20 @@
 import preprocess from "svelte-preprocess";
 import { mdsvex } from "mdsvex";
 import mdsvexConfig from "./mdsvex.config.js";
+import adapter from '@sveltejs/adapter-static';
+
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
     "extensions": [".svelte", ...mdsvexConfig.extensions],
 
     kit: {
 		// hydrate the <div id="svelte"> element in src/app.html
-		target: 'body'
+		target: 'body',
+        adapter: adapter({
+            pages: 'build',
+            assets: 'build',
+            fallback: null
+        }),
 	},
 
     preprocess: [mdsvex(mdsvexConfig), preprocess({})]
