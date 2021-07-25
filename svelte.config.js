580bcc2b243682e19eca82b5d02da6cccf13fe81
nemo-omen <nemo.omen@gmail.com>
2021-07-25 10:09:17 -0500

    Remove unneeded static adapter and gh-pages

--- svelte.config.js
+++ svelte.config.js
@@ -1,7 +1,6 @@
 import preprocess from "svelte-preprocess";
 import { mdsvex } from "mdsvex";
 import mdsvexConfig from "./mdsvex.config.js";
-import adapter from '@sveltejs/adapter-static';
 import netlify from '@sveltejs/adapter-netlify';
 
 /** @type {import('@sveltejs/kit').Config} */
