0b0c050a09d0f7c30f719dbb409fa88564450619
nemo-omen <nemo.omen@gmail.com>
2021-07-25 16:59:11 -0500

    Oops! Wrong heading on /hire

--- src/routes/hire/index.svelte
+++ src/routes/hire/index.svelte
@@ -1,6 +1,6 @@
 <section class="space__outer_top flow">
   <header class="section-header">
-    <h2 class="section-title">About</h2>
+    <h2 class="section-title">Hire</h2>
   </header>
   <!-- <ProjectsSection /> -->
 </section>
